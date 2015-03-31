1f56916cd255950bb7b2fdd2ad33ab3c05168c3b
Connor Peet <connor@peet.io>
2015-03-31 10:39:29 -0400

    Remove /me for now. Sorry Taylor.

--- me/chat/events.js
+++
@@ -1,3 +0,0 @@
-module.exports.hook = function (channel, data) {
-    channel.broadcast('MeEvent', data);
-};
--- me/chat/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (hook) {
-    hook.method('permission', 'me', ['chat', require('./me').hook]);
-    hook.event('MeEvent', require('./events').hook);
-};
