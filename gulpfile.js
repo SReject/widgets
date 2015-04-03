52d2339cc9d74c0f57e1ee52398cacfbb9d0146e
Connor Peet <connor@peet.io>
2015-04-03 10:00:40 -0400

    Add static manifest compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var gulp = require('gulp');
 var widgets = require('./');
+var gulp = require('gulp');
+var fs = require('fs');
+var _ = require('lodash');
 var $ = require('gulp-load-plugins')();
 
-
+/**
+ * Lint runs a linting and code style check on all the widget sources,
+ * exclusing test sources.
+ */
 gulp.task('lint', function () {
     var src = sources('/**/*.js').concat(['!**/*.test.js']);
 
@@ -12,12 +17,51 @@ gulp.task('lint', function () {
         .pipe($.jscs());
 });
 
+/**
+ * Mocha runs the mocha test running on all test/*.js files contained
+ * in widget subdirectories.
+ */
 gulp.task('mocha', function () {
     var src = sources('/test/**/*.js').concat(['test/**/*.js']);
     gulp.src(src, { read: false }).pipe($.mocha());
 });
 
+/**
+ * Cover generates a code coverage report for widgets, using
+ * istanbul.
+ */
+gulp.task('cover', function (done) {
+    var testSrc = sources('/test/**/*.js').concat(['test/**/*.js']);
+    var widgetSrc = sources('/chat/**/*.js').concat(['!**/chat/index.js']);
+
+    gulp.src(widgetSrc)
+        .pipe($.istanbul({ includeUntested: true }))
+        .pipe($.istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(testSrc, { read: false })
+                .pipe($.mocha())
+                .pipe($.istanbul.writeReports())
+                .on('end', done);;
+        });
+});
+
+/**
+ * Buildmanifest pulls out widget metadata into a single external file. This
+ * allows us to include relevant widget data in frontend (Browserify)
+ * Javascript without having to include all the application logic.
+ */
+gulp.task('buildmanifest', function (done) {
+    var data = widgets.map(function (widget) {
+        return _.pick(widget, ['name', 'default', 'authors', 'description', 'permissions']);
+    });
+
+    fs.writeFile('./manifest.json', JSON.stringify(data), done);
+});
+
+// Runs tests on sources.
 gulp.task('test', ['lint', 'mocha']);
+// Runs test on sources and, if they pass, compiles a manifest.
+gulp.task('compile', ['test', 'buildmanifest']);
 
 /**
  * Gets a list of paths for each widget directory, appended with the fragment.
