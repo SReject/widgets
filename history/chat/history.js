2212655ea96c38e1fbba5a4f869002f076e5367e
Jamy Timmermans <me@jamy.be>
2016-02-18 14:31:11 +0100

    Send down history onjoin

--- history/chat/history.js
+++ history/chat/history.js
@@ -10,19 +10,26 @@ let instance = null;
  */
 function History () {
     this.cache = {};
-    
 }
 
 
 
 History.prototype.store = function (channelId, data) {
-    if (!this.cache.hasOwnProperty(channelId)) {
-        this.cache[channelId] = new MsgBuffer(clip.config.messages.cache);
+    if (!this.cache.hasOwnProperty(`c${channelId}`)) {
+        this.cache[`c${channelId}`] = new MsgBuffer(clip.config.messages.cache);
     }
     
-    const historyCache = this.cache[channelId];
+    const historyCache = this.cache[`c${channelId}`];
     
     historyCache.push(data);
+};
+
+History.prototype.getChannelHistory = function (channelId) {
+    if (this.cache.hasOwnProperty(`c${channelId}`)) {
+        return this.cache[`c${channelId}`];
+    }
+    
+    return;
 };
 
 /**
@@ -37,6 +44,27 @@ History.bindChannel = function (channel) {
     channel.on('ChatMessage', function (ch, msg) {
         instance.store(channel.id, msg);
     });
+};
+
+History.bindUser = function (user) {
+    if (!instance) instance = new History();
+    
+    const historyCache = instance.getChannelHistory(user.getChannel().id);
+    if (user.params.history) {
+        const count = Math.min(
+            clip.config.get('messages.cache') || 50, 
+            parseInt(user.params.history, 10) || 0,
+            historyCache.length || 0
+        );
+        // TODO(JamyDev): Export this to a function or use a lodash one?
+        const begin = historyCache.length - count;
+        const slice = historyCache.slice(begin, historyCache.length);
+
+        for (let i = 0; i < slice.length; ++i) {
+            user.socket.sendEvent('ChatMessage', slice[i]);
+        }
+        
+    }
 };
 
 module.exports = History;
--- history/chat/index.js
+++ history/chat/index.js
@@ -2,4 +2,5 @@
 
 module.exports = function (hook) {
     hook.on('channel:new', require('./history').bindChannel);
+    hook.on('user:join', require('./history').bindUser);
 };
