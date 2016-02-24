58840e5a2988c8f5c699d5635612267ed3634ac4
Jamy Timmermans <me@jamy.be>
2016-02-24 22:17:39 +0100

    Fix removed dependecy, added docs

--- history/chat/history.js
+++ history/chat/history.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const MsgBuffer = require('./msgBuffer');
-const LifeRaft = require('liferaft');
 const clip = require('../../clip');
 const _ = require('lodash');
 
@@ -15,9 +14,9 @@ history.store = function (channelId, data) {
     if (!cache.hasOwnProperty(`c${channelId}`)) {
         cache[`c${channelId}`] = new MsgBuffer(clip.config.messages.cache);
     }
-    
+
     const historyCache = cache[`c${channelId}`];
-    
+
     historyCache.push(data);
 };
 
@@ -28,9 +27,9 @@ history.delete = function (channelId, msgId) {
     if (!cache.hasOwnProperty(`c${channelId}`)) {
         return;
     }
-    
+
     const historyCache = cache[`c${channelId}`];
-    
+
     historyCache.remove(msgId);
 };
 
@@ -41,7 +40,7 @@ history.getChannelHistory = function (channelId) {
     if (cache.hasOwnProperty(`c${channelId}`)) {
         return cache[`c${channelId}`];
     }
-    
+
     return;
 };
 
@@ -55,22 +54,22 @@ history.bindChannel = function (channel) {
     channel.on('ChatMessage', function (ch, msg) {
         history.store(channel.id, msg);
     });
-    
+
     channel.on('DeleteMessage', (ch, msgId) => {
         history.delete(channel.id, msgId);
     });
 };
 
-/** 
+/**
  * Handler for the `history` method to get the chat history.
  * @param {Number} args.count Amount of messages to send down.
  */
 history.method = function (socket, args, respond) {
     const count = parseInt(args[0], 10) || 0;
-    
+
     const historyCache = history.getChannelHistory(socket.user.getChannel().id);
     const slice = _.takeRight(historyCache, Math.min(clip.config.get('messages.cache'), count));
-    
+
     respond(null, slice);
 };
 
--- history/chat/msgBuffer.js
+++ history/chat/msgBuffer.js
@@ -2,12 +2,21 @@
 
 const _ = require('lodash');
 
+/**
+ * Array like to store a channel's chat history.
+ */
 class ChannelHistory extends Array {
+    /**
+     * @param  {Number} maxLength Maximum length of the history.
+     */
     constructor(maxLength) {
         super();
         this.maxLength = maxLength || 50;
     }
-
+    /**
+     * Push a message on the array.
+     * @param  {Object} msg The message object.
+     */
     push(msg) {
         if (this.length >= this.maxLength) {
             this.shift();
@@ -16,10 +25,19 @@ class ChannelHistory extends Array {
         Array.prototype.push.call(this, msg);
     }
 
+    /**
+     * Retrieve a message from the array (by id).
+     * @param  {String} id A unique messageID
+     * @return {Message}
+     */
     getMessage(id) {
         return _.find(this, { id });
     }
 
+    /**
+     * Removes a message from the array (by id).
+     * @param  {String} id The messageID
+     */
     remove(id) {
         const i = _.find(this, { id });
         this.splice(i, 1);
