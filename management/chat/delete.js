7ee95e421e5b1da29e2d34f55250f12a61efb55e
Jamy Timmermans <me@jamy.be>
2016-03-01 17:45:46 +0100

    Do deletion per node

--- management/chat/delete.js
+++ management/chat/delete.js
@@ -10,12 +10,12 @@ const del = module.exports = function (socket, args, respond) {
         respond(null, 'Message deleted.');
     }).catch(err => {
         respond(err.message);
-    }); 
+    });
 };
 
 del.remove = function (channelId, messageId, deleterRoles, deleterId) {
     const history = require('../../history/chat/history');
-    
+
     const channelHistory = history.getChannelHistory(channelId);
     if (!channelHistory) {
         throw new Error('Channel not active.');
@@ -24,15 +24,16 @@ del.remove = function (channelId, messageId, deleterRoles, deleterId) {
     if (!msg) {
         throw new Error('Message not found.');
     }
-    
+
     const delRole = clip.roles.getDominant(deleterRoles);
     const msgRole = clip.roles.getDominant(msg.user_roles);
-    
+
     const deletingOwnMessage = msg.user_id === deleterId && delRole.level > clip.roles.getLevel('User');
     if (!clip.roles.canAdministrate(delRole.level, msgRole.level) && !deletingOwnMessage) {
         throw new Error('Access denied.');
     }
-    
+
     return clip.manager.getChannel(channelId)
-    .then(ch => ch.publish('DeleteMessage', { id: messageId }));
+    // Only broadcast locally, since this method is run on each chat instance.
+    .then(ch => ch.broadcast('DeleteMessage', { id: messageId }));
 };
